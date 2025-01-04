9e070e1dd6d37421dd35cf9a24458d71140561d6
outwebfeng <amylindsey958@gmail.com>
2025-01-04 21:35:57 +0800

    添加 ko 语言。

--- i18n.ts
+++ i18n.ts
@@ -1,7 +1,7 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
-// en-US, es-ES, fr-FR, jp-JP, zh-TW
+// en-US, es-ES, fr-FR, jp-JP, zh-TW, ko-KR
 export const languages = [
   {
     code: 'en-US',
@@ -28,9 +28,14 @@ export const languages = [
     lang: 'tw',
     label: '繁體中文',
   },
+  {
+    code: 'ko-KR',
+    lang: 'ko',
+    label: '한국어',
+  },
 ];
 
-export const locales = ['en', 'es', 'fr', 'jp', 'tw'];
+export const locales = ['en', 'es', 'fr', 'jp', 'tw', 'ko'];
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
